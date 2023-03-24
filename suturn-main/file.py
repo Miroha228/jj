8958f712d20a902e4bcbe3bc7778a7202a2cc47b
gpt user <mirosnicenkoartem07@gmail.com>
2023-03-24 20:39:21 +0200

    fd

---
+++ suturn-main/file.py
@@ -0,0 +1,23 @@
+number = 5 
+
+
+digit = 3.1
+word = 'wow you so stupid   '
+boolean = True
+
+str_num = "5"
+
+
+
+# print(word + str(digit));
+print (word + str(number + int(str_num)))
+
+
+
+del number 
+
+number = 7
+print ("Resut =", number);
+
+
+
